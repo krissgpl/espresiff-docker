3c24de720b4c7e6eeff9c7cc4684812614043999
krissgpl <kris.sg@wp.pl>
2024-07-28 14:03:27 +0200

    error fix 2

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PROJ=~/CProjects
 [ -e $PROJ ] || mkdir -p $PROJ
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
-docker run --name devel/esp8266 -v "$PROJ":/CProjects -it --name devel/esp8266 /bin/bash
+docker run --name devel/esp8266 -v "$PROJ":/CProjects -it devel/esp8266 /bin/bash
