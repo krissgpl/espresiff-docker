1f7b5fb4a1b4ea1c26d7f74bdbeb3b281f4bed5e
krissgpl <kris.sg@wp.pl>
2024-07-28 13:56:16 +0200

    set docker name

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PROJ=~/CProjects
 [ -e $PROJ ] || mkdir -p $PROJ
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
-docker run -v "$PROJ":/CProjects -it devel/esp8266 /bin/bash
+docker run -v "$PROJ":/CProjects --name devel/esp8266 -it /bin/bash
