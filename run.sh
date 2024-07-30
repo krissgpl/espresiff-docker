53495787b7da5d9eae02818ba2bece6e98ca7caa
krissgpl <kris.sg@wp.pl>
2024-07-30 06:00:03 +0200

    remove container before start

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ PROJ=~/CProjects
 [ -e $PROJ ] || mkdir -p $PROJ
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
+docker rm devel-esp8266
 docker run --name devel-esp8266 -v "$PROJ":/CProjects -it devel/esp8266 /bin/bash
