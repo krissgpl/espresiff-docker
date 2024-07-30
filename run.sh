b40af04e54842c6935e5281d8c8df2d0b8197d00
krissgpl <kris.sg@wp.pl>
2024-07-30 07:34:26 +0200

    missing switch

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ ROOT=~/CProjects/root
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
 docker rm devel-esp8266
-docker run --name devel-esp8266 -v "$PROJ":/CProjects "$ROOT":/root -it devel/esp8266 /bin/bash
+docker run --name devel-esp8266 -v "$PROJ":/CProjects -v "$ROOT":/root -it devel/esp8266 /bin/bash
