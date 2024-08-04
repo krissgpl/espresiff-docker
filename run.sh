e4f8e931716102be73f90cb016b73349204f702a
krissgpl <kris.sg@wp.pl>
2024-08-04 13:48:29 +0200

    final version

--- run.sh
+++ run.sh
@@ -7,23 +7,9 @@ FIRM=/media/QNAP/ESP_Firmware
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
 
-#if docker container ls -a | grep -q devel-esp8266
-#then 
-#    echo "Running - devel-esp8266!"
-#	docker rm devel-esp8266
-#fi
-
 if docker inspect "/devel-esp8266" > /dev/null 2>&1; then
-    echo "The container devel-8266 exists."
+    echo "The container devel-8266 exists. Remove it"
 	docker rm devel-esp8266
-fi
-
-if [ ! "$(docker ps -a -q -f name=devel/esp8266)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=devel/esp8266)" ]; then
-        # cleanup
-		echo "Running - devel-esp8266!"
-        #docker rm devel-esp8266
-    fi
 fi
 
 docker run --name devel-esp8266 -v "$PROJ":/CProjects -v "$ROOT":/root -v "$FIRM":/ESP_Firmware -it devel/esp8266 /bin/bash
