7e7abd21ccf443d0f718aefaec78aff6e7016782
krissgpl <kris.sg@wp.pl>
2024-08-04 13:31:15 +0200

    update docker name chceck

--- run.sh
+++ run.sh
@@ -6,9 +6,18 @@ FIRM=/media/QNAP/ESP_Firmware
 [ -e $PROJ ] || mkdir -p $PROJ
 cd $PROJ
 [ ! -e ./supla-espressif-esp ] && git clone https://github.com/krissgpl/supla-espressif-esp
-if docker ps | grep -q devel-esp8266
-then 
-    echo "Running - devel-esp8266!"
-	docker rm devel-esp8266
+
+#if docker container ls -a | grep -q devel-esp8266
+#then 
+#    echo "Running - devel-esp8266!"
+#	docker rm devel-esp8266
+#fi
+
+if [ ! "$(docker ps -a -q -f name=devel-esp8266)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=devel-esp8266)" ]; then
+        # cleanup
+        docker rm devel-esp8266
+    fi
 fi
+
 docker run --name devel-esp8266 -v "$PROJ":/CProjects -v "$ROOT":/root -v "$FIRM":/ESP_Firmware -it devel/esp8266 /bin/bash
