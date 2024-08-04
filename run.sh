00fee56d000dba26b7f0ade84164a1f2558a3654
krissgpl <kris.sg@wp.pl>
2024-08-04 13:33:29 +0200

    error fix

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ cd $PROJ
 #	docker rm devel-esp8266
 #fi
 
-if [ ! "$(docker ps -a -q -f name=devel-esp8266)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=devel-esp8266)" ]; then
+if [ ! "$(docker ps -a -q -f name=/devel-esp8266)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=/devel-esp8266)" ]; then
         # cleanup
         docker rm devel-esp8266
     fi
